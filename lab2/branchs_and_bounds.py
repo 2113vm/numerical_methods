04653c120b66a23f5556a08c7a4d00568245ff5b
googolplex <21138532110q@gmail.com>
2018-04-26 18:34:23 +0500

    change data in lab 2-3

--- lab2/branchs_and_bounds.py
+++ lab2/branchs_and_bounds.py
@@ -1,10 +1,9 @@
 """
 решить задачу с помощью метода ветвей и границ
-f = 21*x1 + 35*x2 + 50*x3
-f -> min
-g1 = 2.5*x1 + 5*x2 + 10*x3 <= 40
-g2 = 2.5*x1 + 5*x2 + 10*x3 >= 35
-
+# f = 17*x1 + 30*x2 + 75*x3;
+# f -> min;
+# g1 = 1.0*x1 + 2.2*x2 + 4*x3 <= 15;
+# g2 = 1.0*x1 + 2.2*x2 + 4*x3 >= 13;
 """
 
 from pulp import *
@@ -15,10 +14,9 @@ x1 = LpVariable("x1", 0, 10, 'Integer')
 x2 = LpVariable("x2", 0, 10, 'Integer')
 x3 = LpVariable("x3", 0, 10, 'Integer')
 
-prob += 21*x1 + 35*x2 + 50*x3, "obj"
-prob += 2.5*x1 + 5*x2 + 10*x3 <= 40
-prob += 2.5*x1 + 5*x2 + 10*x3 >= 35
-
+prob += 17*x1 + 30*x2 + 75*x3, 'obj'
+prob += 1.0*x1 + 2.2*x2 + 4*x3 <= 15
+prob += 1.0*x1 + 2.2*x2 + 4*x3 >= 13
 prob.solve()
 
 print("Status:", LpStatus[prob.status])
