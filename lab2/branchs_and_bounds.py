ff161210f88f47c4588d2b293c605f3454562257
googolplex <21138532110q@gmail.com>
2018-04-26 11:26:08 +0500

    add doc string

--- lab2/branchs_and_bounds.py
+++ lab2/branchs_and_bounds.py
@@ -1,11 +1,16 @@
-# -*- coding: cp1251 -*-
-# импортируем функции PuLP
+"""
+СЂРµС€РёС‚СЊ Р·Р°РґР°С‡Сѓ СЃ РїРѕРјРѕС‰СЊСЋ РјРµС‚РѕРґР° РІРµС‚РІРµР№ Рё РіСЂР°РЅРёС†
+f = 21*x1 + 35*x2 + 50*x3
+f -> min
+g1 = 2.5*x1 + 5*x2 + 10*x3 <= 40
+g2 = 2.5*x1 + 5*x2 + 10*x3 >= 35
+
+"""
+
 from pulp import *
 
-# Создаем новую задачу Линейного программирования (LP) с максимизацией целевой функции
 prob = LpProblem("Knapsack problem", LpMinimize)
 
-# Переменные оптимизации, целые
 x1 = LpVariable("x1", 0, 10, 'Integer')
 x2 = LpVariable("x2", 0, 10, 'Integer')
 x3 = LpVariable("x3", 0, 10, 'Integer')
@@ -16,12 +21,9 @@ prob += 2.5*x1 + 5*x2 + 10*x3 >= 35
 
 prob.solve()
 
-# Выводим статус задачи
 print("Status:", LpStatus[prob.status])
 
-# Выводим получившиеся оптимальные значения переменных
 for v in prob.variables():
     print(v.name, "=", v.varValue)
 
-# Выводим оптимальное значение целевой функции
 print("objective = %s$" % value(prob.objective))
